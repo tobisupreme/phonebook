d1cbd0c87e5eeb5585cedaf05b64e22d03da31f3
Tobi Balogun <tobi.balo@gmail.com>
2022-09-12 23:14:33 +0100

    implement functionality to add names to phonebook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import { useState } from 'react'
 
+const Display = ({ name }) => {
+  return (
+    <>
+      <span>{name}</span>
+      <br />
+    </>
+  )
+}
+
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'tobisupreme' }])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
+
+  const handleNewName = (e) => {
+    setNewName(e.target.value)
+  }
+
+  const saveName = (e) => {
+    e.preventDefault()
+    const newPerson = {
+      name: newName,
+    }
+    setPersons(persons.concat(newPerson))
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={saveName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
         <h2>Numbers</h2>
       </form>
+
+      <div>
+        {persons.map((person) => {
+          return <Display key={person.name} name={person.name} />
+        })}
+      </div>
 
       <div>debug: {newName}</div>
     </div>
